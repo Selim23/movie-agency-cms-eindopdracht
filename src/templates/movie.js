e22bd443f653941ae3e2dae82c69181962246b0b
Selim23 <60688990+Selim23@users.noreply.github.com>
2021-01-23 06:07:49 +0100

    dynamic page helemaa klaar

--- src/templates/movie.js
+++ src/templates/movie.js
@@ -4,13 +4,45 @@ import Layout from '../components/Layout'
 import SEO from '../components/Seo'
 import {Wrapper, Image} from './templateStyles/movieStyles'
 
-const Movie = ({data}) => {
-    console.log(data)
-    return (
-        <div>
-            Hello
-        </div>
-    )
+const Movie = ({
+    data:{
+        wpcontent:{
+            movie:{
+                film,
+                genres:{ edges: genres}
+            }
+        }
+    }
+}) => {
+    return <Layout>
+        <SEO title ="Movie" />
+        <Wrapper>
+            <div className ="artist-container">
+                <div className="artist-image">
+                <Image fluid={film.image.imageFile.childImageSharp.fluid} alt={film.image.altText} />
+                <div className="roles">
+                    {genres.map(({node: genre}) => (
+                        <div className="role">
+                            {genre.name}
+                        </div>
+                    ))}
+                </div>
+                </div>
+                <div className="artist-info">
+                    <h2>{film.naam}</h2>
+                    <h3><span>{film.releasejaar}</span> - <span>{film.hoofdpersonage}</span></h3>
+                    <h3><span>{film.beschrijving}</span></h3>
+                    <p className="info">
+                        <strong className="description">Cast: </strong>{film.hoofdpersonage}
+                    </p>
+                    <p className="info">
+                        <strong className="description">Release Date: </strong>{film.releasejaar}
+                    </p>
+                </div>
+            </div>
+        </Wrapper>
+    </Layout>
+    
 }
 
 export default Movie
